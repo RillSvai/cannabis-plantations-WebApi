a832ad9f6ab679a3c81e06396cae8f2dd9f69a62
RillSvai <kirilkvas07@gmail.com>
2023-11-29 15:14:36 +0200

    Ended with get and getall for last two controllers

--- CannabisPlantations.WebApi/Controllers/V1/ProductStorageController.cs
+++ CannabisPlantations.WebApi/Controllers/V1/ProductStorageController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using CannabisPlantations.WebApi.Data.Repositories.IRepositories;
+using CannabisPlantations.WebApi.Filters.V1.ActionFilters;
+using CannabisPlantations.WebApi.Filters.V1.ActionFilters.ProductActionFilters;
+using CannabisPlantations.WebApi.Models.Dtos;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CannabisPlantations.WebApi.Controllers.V1
@@ -8,5 +13,31 @@ namespace CannabisPlantations.WebApi.Controllers.V1
     [ApiVersion("1.0")]
     public class ProductStorageController : ControllerBase
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public ProductStorageController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<ProductStorageDto>> GetAll() 
+        {
+            IEnumerable<ProductStorageDto> productStorageDtos = _mapper.Map<IEnumerable<ProductStorageDto>>(_unitOfWork.ProductStorageRepo.GetAll());
+            return Ok(productStorageDtos);
+        }
+        [HttpGet("{productStorageId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [IdFilter]
+        [TypeFilter(typeof(ProductStorageExistFilterAttribute))]
+        public ActionResult<ProductStorageDto> Get([FromRoute] int productStorageId) 
+        {
+            ProductStorageDto productStorageDto = _mapper.Map<ProductStorageDto>(HttpContext.Items["productStorage"]);
+            return Ok(productStorageDto);
+        }
     }
 }
--- CannabisPlantations.WebApi/Controllers/V1/ReturnController.cs
+++ CannabisPlantations.WebApi/Controllers/V1/ReturnController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using CannabisPlantations.WebApi.Data.Repositories.IRepositories;
+using CannabisPlantations.WebApi.Filters.V1.ActionFilters.ProductActionFilters;
+using CannabisPlantations.WebApi.Filters.V1.ActionFilters;
+using CannabisPlantations.WebApi.Models.Dtos;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using CannabisPlantations.WebApi.Filters.V1.ActionFilters.ReturnActionFilters;
 
 namespace CannabisPlantations.WebApi.Controllers.V1
 {
@@ -8,5 +14,31 @@ namespace CannabisPlantations.WebApi.Controllers.V1
     [ApiVersion("1.0")]
     public class ReturnController : ControllerBase
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public ReturnController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<ReturnDto>> GetAll()
+        {
+            IEnumerable<ReturnDto> returnDtos = _mapper.Map<IEnumerable<ReturnDto>>(_unitOfWork.ReturnRepo.GetAll());
+            return Ok(returnDtos);
+        }
+        [HttpGet("{returnId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [IdFilter]
+        [TypeFilter(typeof(ReturnExistFilterAttribute))]
+        public ActionResult<ReturnDto> Get([FromRoute] int returnId)
+        {
+            ReturnDto returnDto = _mapper.Map<ReturnDto>(HttpContext.Items["return"]);
+            return Ok(returnDto);
+        }
     }
 }
